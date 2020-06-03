724c98f7fa441a81de134d7ea69f95bd0cf3cc30
Kr328 <39107975+Kr328@users.noreply.github.com>
2020-06-03 11:40:09 +0800

    remove heap impl for available

--- binder_bus.cpp
+++ binder_bus.cpp
@@ -6,8 +6,6 @@ void BinderBus::registerBinder(BnBinder *binder) {
     std::lock_guard<std::shared_mutex> lock(mutex);
 
     if ( !available.empty() ) {
-        std::push_heap(available.begin(), available.end(), std::greater<>{});
-
         int id = available.back();
 
         available.pop_back();
@@ -27,8 +25,6 @@ void BinderBus::unregisterBinder(BnBinder *binder) {
     binders[binder->id] = nullptr;
 
     available.push_back(binder->id);
-
-    std::push_heap(available.begin(), available.end());
 
     binder->id = -1;
 }
